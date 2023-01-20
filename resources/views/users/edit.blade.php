28b67e49197f0140b252dae95635e61041dcb9c8
luthfitabey <37507037+luthfitabey@users.noreply.github.com>
2023-01-20 10:51:41 +0700

    fixing edit user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Edit User')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Edit User</h1>
+    <!-- <h1 class="m-0 text-dark">Edit User</h1> -->
 @stop
 
 @section('content')
